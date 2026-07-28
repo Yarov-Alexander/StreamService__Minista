9da9b86c7c6f25533cca670ab99d316c64531944
Alex <aaaaesvd@gmail.com>
2026-07-28 23:16:39 +0300

    fix

--- src/sections/Devices/Devices.jsx
+++ src/sections/Devices/Devices.jsx
@@ -7,32 +7,32 @@ const Devices = () => {
     {
       title: 'Smartphones',
       description: 'StreamVibe is optimized for both Android and iOS smartphones. Download our app from the Google Play Store or the Apple App Store',
-      imgSrc: '/devices/1.svg',
+      imgSrc: `${import.meta.env.BASE_URL}devices/1.svg`,
     },
     {
       title: 'Tablet',
       description: 'StreamVibe is optimized for both Android and iOS smartphones. Download our app from the Google Play Store or the Apple App Store',
-      imgSrc: '/devices/2.svg',
+      imgSrc: `${import.meta.env.BASE_URL}devices/2.svg`,
     },
     {
       title: 'Smart TV',
       description: 'StreamVibe is optimized for both Android and iOS smartphones. Download our app from the Google Play Store or the Apple App Store',
-      imgSrc: '/devices/3.svg',
+      imgSrc: `${import.meta.env.BASE_URL}devices/3.svg`,
     },
     {
       title: 'Laptops',
       description: 'StreamVibe is optimized for both Android and iOS smartphones. Download our app from the Google Play Store or the Apple App Store',
-      imgSrc: '/devices/4.svg',
+      imgSrc: `${import.meta.env.BASE_URL}devices/4.svg`,
     },
     {
       title: 'Gaming Consoles',
       description: 'StreamVibe is optimized for both Android and iOS smartphones. Download our app from the Google Play Store or the Apple App Store',
-      imgSrc: '/devices/4.svg',
+      imgSrc: `${import.meta.env.BASE_URL}devices/4.svg`,
     },
     {
       title: 'VR Headsets',
       description: 'StreamVibe is optimized for both Android and iOS smartphones. Download our app from the Google Play Store or the Apple App Store',
-      imgSrc: '/devices/5.svg',
+      imgSrc: `${import.meta.env.BASE_URL}devices/5.svg`,
     },
   ]
 
