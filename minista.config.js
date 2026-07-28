8b8d10879f575fc05a6ab96f533331b6949b6fe5
Alex <aaaaesvd@gmail.com>
2026-07-28 20:59:39 +0300

    deploy project

--- minista.config.js
+++ minista.config.js
@@ -3,7 +3,7 @@ import path from 'path'
 
 export default defineConfig({
   root: '',
-  base: '/',
+  base: '/StreamService__Minista/',
   public: 'public',
   out: 'dist',
   assets: {
--- src/sections/Collections/collectionGroups.js
+++ src/sections/Collections/collectionGroups.js
@@ -16,40 +16,40 @@ const collectionGroups = [
             title: 'Action',
             badge: 'Top 10 In',
             images: [
-              '/src/assets/images/categories/action/1.jpg',
-              '/src/assets/images/categories/action/2.jpg',
-              '/src/assets/images/categories/action/3.jpg',
-              '/src/assets/images/categories/action/4.jpg',
+              '/src/assets/images/categories/top_10/action/1.jpg',
+              '/src/assets/images/categories/top_10/action/2.jpg',
+              '/src/assets/images/categories/top_10/action/3.jpg',
+              '/src/assets/images/categories/top_10/action/4.jpg',
             ],
           },
           {
             title: 'Adventure',
             badge: 'Top 10 In',
             images: [
-              '/src/assets/images/categories/adventure/1.jpg',
-              '/src/assets/images/categories/adventure/2.jpg',
-              '/src/assets/images/categories/adventure/3.jpg',
-              '/src/assets/images/categories/adventure/4.jpg',
+              '/src/assets/images/categories/top_10/adventure/1.jpg',
+              '/src/assets/images/categories/top_10/adventure/2.jpg',
+              '/src/assets/images/categories/top_10/adventure/3.jpg',
+              '/src/assets/images/categories/top_10/adventure/4.jpg',
             ],
           },
           {
             title: 'Comedy',
             badge: 'Top 10 In',
             images: [
-              '/src/assets/images/categories/comedy/1.jpg',
-              '/src/assets/images/categories/comedy/2.jpg',
-              '/src/assets/images/categories/comedy/3.jpg',
-              '/src/assets/images/categories/comedy/4.jpg',
+              '/src/assets/images/categories/top_10/comedy/1.jpg',
+              '/src/assets/images/categories/top_10/comedy/2.jpg',
+              '/src/assets/images/categories/top_10/comedy/3.jpg',
+              '/src/assets/images/categories/top_10/comedy/4.jpg',
             ],
           },
           {
             title: 'Drama',
             badge: 'Top 10 In',
             images: [
-              '/src/assets/images/categories/drama/1.jpg',
-              '/src/assets/images/categories/drama/2.jpg',
-              '/src/assets/images/categories/drama/3.jpg',
-              '/src/assets/images/categories/drama/4.jpg',
+              '/src/assets/images/categories/top_10/drama/1.jpg',
+              '/src/assets/images/categories/top_10/drama/2.jpg',
+              '/src/assets/images/categories/top_10/drama/3.jpg',
+              '/src/assets/images/categories/top_10/drama/4.jpg',
             ],
           },
 
@@ -57,9 +57,9 @@ const collectionGroups = [
             title: 'Action 2',
             badge: 'Top 10 In',
             images: [
-              '/src/assets/images/categories/action/1.jpg',
-              '/src/assets/images/categories/action/2.jpg',
-              '/src/assets/images/categories/action/3.jpg',
+              '/src/assets/images/categories/top_10/action/1.jpg',
+              '/src/assets/images/categories/top_10/action/2.jpg',
+              '/src/assets/images/categories/top_10/action/3.jpg',
               '/src/assets/images/categories/action/4.jpg',
             ],
           },
@@ -211,26 +211,26 @@ const collectionGroups = [
         title: 'Trending Now',
         movieItems: [
           {
-            title: 'Morbius',
-            imgSrc: '/src/assets/images/movies/1.jpg',
+            title: 'Stranger things',
+            imgSrc: '/src/assets/images/categories/trending/1.jpg',
+            duration: '1h 30min',
+            views: '2K',
+          },
+          {
+            title: 'Money heist',
+            imgSrc: '/src/assets/images/categories/trending/2.jpg',
             duration: '1h 30min',
             views: '2K',
           },
           {
             title: 'Morbius',
-            imgSrc: '/src/assets/images/movies/2.jpg',
+            imgSrc: '/src/assets/images/categories/trending/3.jpg',
             duration: '1h 30min',
             views: '2K',
           },
           {
             title: 'Morbius',
-            imgSrc: '/src/assets/images/movies/3.jpg',
-            duration: '1h 30min',
-            views: '2K',
-          },
-          {
-            title: 'Morbius',
-            imgSrc: '/src/assets/images/movies/4.jpg',
+            imgSrc: '/src/assets/images/categories/trending/4.jpg',
             duration: '1h 30min',
             views: '2K',
           },
@@ -507,8 +507,8 @@ const collectionGroups = [
         title: 'Must - Watch Movies',
         movieItems: [
           {
-            title: 'Morbius',
-            imgSrc: '/src/assets/images/movies/1.jpg',
+            title: 'Adipurush',
+            imgSrc: '/src/assets/images/categories/must_watch/1.jpg',
             duration: '1h 57min',
             rating: {
               value: 4.5,
@@ -517,7 +517,7 @@ const collectionGroups = [
           },
           {
             title: 'Morbius',
-            imgSrc: '/src/assets/images/movies/1.jpg',
+            imgSrc: '/src/assets/images/categories/must_watch/2.jpg',
             duration: '1h 57min',
             rating: {
               value: 4.5,
@@ -526,7 +526,7 @@ const collectionGroups = [
           },
           {
             title: 'Morbius',
-            imgSrc: '/src/assets/images/movies/1.jpg',
+            imgSrc: '/src/assets/images/categories/must_watch/3.jpg',
             duration: '1h 57min',
             rating: {
               value: 4.5,
@@ -535,7 +535,7 @@ const collectionGroups = [
           },
           {
             title: 'Morbius',
-            imgSrc: '/src/assets/images/movies/1.jpg',
+            imgSrc: '/src/assets/images/categories/must_watch/4.jpg',
             duration: '1h 57min',
             rating: {
               value: 4.5,
@@ -897,28 +897,28 @@ const collectionGroups = [
         movieItems: [
           {
             title: 'Morbius',
-            imgSrc: '/src/assets/images/movies/1.jpg',
+            imgSrc: '/src/assets/images/categories/trending/1.jpg',
             duration: '1h 30min',
             season: '4 Season',
             href: '/show',
           },
           {
             title: 'Morbius',
-            imgSrc: '/src/assets/images/movies/1.jpg',
+            imgSrc: '/src/assets/images/categories/trending/2.jpg',
             duration: '1h 30min',
             season: '4 Season',
             href: '/show',
           },
           {
             title: 'Morbius',
-            imgSrc: '/src/assets/images/movies/1.jpg',
+            imgSrc: '/src/assets/images/categories/trending/3.jpg',
             duration: '1h 30min',
             season: '4 Season',
             href: '/show',
           },
           {
             title: 'Morbius',
-            imgSrc: '/src/assets/images/movies/1.jpg',
+            imgSrc: '/src/assets/images/categories/trending/4.jpg',
             duration: '1h 30min',
             season: '4 Season',
             href: '/show',
@@ -926,14 +926,14 @@ const collectionGroups = [
 
           {
             title: 'Morbius',
-            imgSrc: '/src/assets/images/movies/1.jpg',
+            imgSrc: '/src/assets/images/categories/trending/1.jpg',
             duration: '1h 30min',
             season: '4 Season',
             href: '/show',
           },
           {
             title: 'Morbius',
-            imgSrc: '/src/assets/images/movies/1.jpg',
+            imgSrc: '/src/assets/images/categories/trending/2.jpg',
             duration: '1h 30min',
             season: '4 Season',
             href: '/show',
@@ -1047,28 +1047,28 @@ const collectionGroups = [
         movieItems: [
           {
             title: 'Morbius',
-            imgSrc: '/src/assets/images/movies/1.jpg',
+            imgSrc: '/src/assets/images/categories/must_watch/1.jpg',
             duration: '1h 30min',
             season: '4 Season',
             href: '/show',
           },
           {
             title: 'Morbius',
-            imgSrc: '/src/assets/images/movies/1.jpg',
+            imgSrc: '/src/assets/images/categories/must_watch/2.jpg',
             duration: '1h 30min',
             season: '4 Season',
             href: '/show',
           },
           {
             title: 'Morbius',
-            imgSrc: '/src/assets/images/movies/1.jpg',
+            imgSrc: '/src/assets/images/categories/must_watch/3.jpg',
             duration: '1h 30min',
             season: '4 Season',
             href: '/show',
           },
           {
             title: 'Morbius',
-            imgSrc: '/src/assets/images/movies/1.jpg',
+            imgSrc: '/src/assets/images/categories/must_watch/4.jpg',
             duration: '1h 30min',
             season: '4 Season',
             href: '/show',
@@ -1076,7 +1076,7 @@ const collectionGroups = [
 
           {
             title: 'Morbius',
-            imgSrc: '/src/assets/images/movies/1.jpg',
+            imgSrc: '/src/assets/images/categories/trending/1.jpg',
             duration: '1h 30min',
             season: '4 Season',
             href: '/show',
@@ -1197,7 +1197,7 @@ const collectionGroups = [
         movieItems: [
           {
             title: 'Morbius',
-            imgSrc: '/src/assets/images/movies/1.jpg',
+            imgSrc: '/src/assets/images/categories/must_watch_2/1.jpg',
             duration: '1h 57min',
             rating: {
               value: 4.5,
@@ -1206,7 +1206,7 @@ const collectionGroups = [
           },
           {
             title: 'Morbius',
-            imgSrc: '/src/assets/images/movies/1.jpg',
+            imgSrc: '/src/assets/images/categories/must_watch/2.jpg',
             duration: '1h 57min',
             rating: {
               value: 4.5,
@@ -1215,7 +1215,7 @@ const collectionGroups = [
           },
           {
             title: 'Morbius',
-            imgSrc: '/src/assets/images/movies/1.jpg',
+            imgSrc: '/src/assets/images/movies/6.jpg',
             duration: '1h 57min',
             rating: {
               value: 4.5,
@@ -1224,7 +1224,7 @@ const collectionGroups = [
           },
           {
             title: 'Morbius',
-            imgSrc: '/src/assets/images/movies/1.jpg',
+            imgSrc: '/src/assets/images/categories/must_watch_2/4.jpg',
             duration: '1h 57min',
             rating: {
               value: 4.5,
@@ -1234,7 +1234,7 @@ const collectionGroups = [
 
           {
             title: 'Morbius',
-            imgSrc: '/src/assets/images/movies/1.jpg',
+            imgSrc: '/src/assets/images/categories/must_watch_2/1.jpg',
             duration: '1h 57min',
             rating: {
               value: 4.5,
